db646ab392ce10b7fa1e9eb69df66a13693f010f
Nitish <rdpara1214@gmail.com>
2022-10-19 16:54:29 +0530

    added playlist in dashboard

--- src/login/login.js
+++ src/login/login.js
@@ -16,7 +16,7 @@ const authorizeUser = () => {
 const setItemInLocalStorage = ({ accessToken, tokenType, expiresIn }) => {
   localStorage.setItem(ACCESS_TOKEN, accessToken);
   localStorage.setItem(TOKEN_TYPE, tokenType);
-  localStorage.setItem(EXPIRES_IN, expiresIn);
+  localStorage.setItem(EXPIRES_IN, Date.now() + expiresIn * 1000);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
       colors: {
         green: "#1db954",
         black: "#191414",
+        primary: "#FFF",
+        gray: "#535353",
+        secondary: "#b3b3b3",
+        "light-black": "#232323",
+      },
+      gridTemplateColumns:{
+        'auto-fill-cards':'repeat(auto-fill,minmax(200px,1fr))',
       },
     },
   },
